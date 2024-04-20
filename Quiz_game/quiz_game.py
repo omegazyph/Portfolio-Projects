a8f992fe1889f0d9e42285581bcc5ccbf3187529
omegazyph <omegazyph@gmail.com>
2024-04-20 18:37:38 -0400

    added comments and readme page

--- Quiz_game/quiz_game.py
+++ Quiz_game/quiz_game.py
@@ -1,20 +1,26 @@
+# Welcome message
 print("Welcome to my computer quiz!")
 
+# Asking if the player wants to play
 playing = input("Do you want to play? ")
 
+# Exiting the program if the player doesn't want to play
 if playing.lower() != "yes":
     quit()
 
-print("Okay! let's play :)")
-score  = 0
+# Starting the quiz
+print("Okay! Let's play :)")
+score = 0  # Initializing the score variable to keep track of correct answers
 
+# Asking the first question
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
-    print("Correct!")
-    score += 1
+    print("Correct!")  # Providing feedback for correct answer
+    score += 1  # Incrementing the score for correct answer
 else:
-    print("Incorrect!")
+    print("Incorrect!")  # Providing feedback for incorrect answer
 
+# Asking the second question
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
     print("Correct!")
@@ -22,6 +28,7 @@ if answer.lower() == "graphics processing unit":
 else:
     print("Incorrect!")
 
+# Asking the third question
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
     print("Correct!")
@@ -29,6 +36,7 @@ if answer.lower() == "random access memory":
 else:
     print("Incorrect!")
 
+# Asking the fourth question
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply":
     print("Correct!")
@@ -36,6 +44,6 @@ if answer.lower() == "power supply":
 else:
     print("Incorrect!")
 
-
+# Displaying the final score
 print(" You got " + str(score) + " questions correct")
 print(" You got " + str((score/4)*100) + "%. correct")
