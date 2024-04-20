d9fdffd2bd4bcc743abb9e8fc1e954389daff1b1
omegazyph <omegazyph@gmail.com>
2024-04-20 19:01:49 -0400

    added color

--- Password_manger/Password_manger_V3.0/Password_manger_V3.0.py
+++ Password_manger/Password_manger_V3.0/Password_manger_V3.0.py
@@ -5,12 +5,19 @@ class Application(tk.Tk):
         super().__init__()
         self.title("Password Manager V3.0")
         self.geometry("400x300")  # Set the initial size of the window
+        self.configure(background="#f0f0f0")  # Set background color
 
         # Create widgets
-        self.label = tk.Label(self, text="Hello, Tkinter!", font=("Helvetica", 16))
+        self.label = tk.Label(self, text="Hello, Tkinter!", 
+                              font=("Helvetica", 16), 
+                              bg="#f0f0f0", 
+                              fg="#333333")  # Set background and foreground color
         self.label.pack(pady=20)
 
-        self.button = tk.Button(self, text="Click Me!", command=self.on_button_click)
+        self.button = tk.Button(self, text="Click Me!", 
+                                command=self.on_button_click, 
+                                bg="#008CBA", 
+                                fg="white")  # Set button colors
         self.button.pack()
 
     def on_button_click(self):
